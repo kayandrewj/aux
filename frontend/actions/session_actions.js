63db36fb90689d4b73511802c80928bc7532b432
andrewjkay <kayandrewj@gmail.com>
2017-08-23 15:36:36 -0400

    fix nav name change import

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -43,6 +43,6 @@ export const login = user => dispatch => {
 
 export const logout = () => dispatch => {
   SessionApiUtil.logout().then(
-    currentUser => dispatch(receiveCurrentUser(currentUser))
+    currentUser => dispatch(receiveCurrentUser(null))
   );
 };
