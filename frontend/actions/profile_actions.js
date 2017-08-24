36e078ce1e02aee522120e35dfc74a89b40d4e46
andrewjkay <kayandrewj@gmail.com>
2017-08-24 13:58:32 -0400

    initial commit profile show page

---
+++ frontend/actions/profile_actions.js
@@ -0,0 +1,23 @@
+//actions: RECEIVE_PROFILE_EDIT, RECEIVE_PROFILE_ERRORS
+
+export const RECEIVE_PROFILE_EDIT = "RECEIVE_PROFILE_EDIT";
+
+
+
+
+
+
+
+
+
+
+
+
+
+// need edit action
+// if edit is happening on profile reducer this also needs action to send
+// new user props to the back end.
+
+// do users deserve to change their information
+
+// that's pretty much it.
