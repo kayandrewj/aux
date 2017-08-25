bb391c9c00ac8ee483dc9feafebfedb3f7db37d0
andrewjkay <kayandrewj@gmail.com>
2017-08-25 14:49:12 -0400

    reconfigure user routes to hold albums

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, default: {format: :json} do
-    resources :users, only: [:create]
+    resources :users, only: [:create] do
+      resources :albums, only: [:index, :create, :destroy, :edit]
+    end
     resource :session, only: [:create, :destroy]
-
+    resources :albums, only: [:show]
   end
+
+
 
 end
