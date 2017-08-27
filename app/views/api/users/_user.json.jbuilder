913809ac43fd9cac8a81f0766889c367a542f94b
andrewjkay <kayandrewj@gmail.com>
2017-08-27 15:41:47 -0400

    add location to users

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract!(user, :id, :username, :avatar, :bio)
+json.extract!(user, :id, :username, :avatar, :bio, :location)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827190517) do
+ActiveRecord::Schema.define(version: 20170827193138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170827190517) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.text "bio"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
