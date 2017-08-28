725e414ef9e67edf27a8ce97d693f60441f255c5
andrewjkay <kayandrewj@gmail.com>
2017-08-28 12:08:48 -0400

    refactor album route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -11,11 +11,11 @@ const App = () => {
   return (
     <div className='app'>
 
-
-
+        <Route path="/album/:albumId" component={AlbumContainer} />
         <Route path="/profile/:userId" component={ProfileContainer} />
         <Route exact path="/band" component={SessionFormContainer} />
         <Route path="/" component={NavContainer} />
+
         <AuthRoute exact path="/signup" component={SessionFormContainer} />
         <AuthRoute exact path="/login" component={SessionFormContainer} />
         <AuthRoute exact path="/band" component={SessionFormContainer} />
@@ -25,8 +25,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-// <Route path="/album" component={AlbumContainer} />
