c43fc694454c72a8f201fb9cfcc6648daddcd970
andrewjkay <kayandrewj@gmail.com>
2017-09-01 00:19:07 -0400

    fix header image size

--- app/models/album.rb
+++ app/models/album.rb
@@ -18,7 +18,7 @@ class Album < ApplicationRecord
   multisearchable against: %i(title user)
 
   validates :user_id, :title, presence: true
-  has_attached_file :artwork, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://s.discogs.com/images/default-label.png"
+  has_attached_file :artwork, styles: { medium: "352x352>", thumb: "100x100>" }, default_url: "https://s.discogs.com/images/default-label.png"
   validates_attachment_content_type :artwork, content_type: /\Aimage\/.*\Z/
 
   belongs_to :user
--- app/models/artist_profile.rb
+++ app/models/artist_profile.rb
@@ -20,7 +20,7 @@ class ArtistProfile < ApplicationRecord
 
   belongs_to :user
 
-  has_attached_file :header, styles: { medium: "300x300>", thumb: "100x100>", header:"180x1000>" }, default_url: "http://i.imgur.com/t2pGWHv.jpg"
+  has_attached_file :header, styles: { medium: "300x300>", thumb: "100x100>", large: "180x1000>" }, default_url: "http://i.imgur.com/t2pGWHv.jpg"
   validates_attachment_content_type :header, content_type: /\Aimage\/.*\Z/
 
 end
