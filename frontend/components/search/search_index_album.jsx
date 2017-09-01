e7d8aaa31e2e8229ab2baf54c6c231b892659fbd
andrewjkay <kayandrewj@gmail.com>
2017-09-01 05:34:03 -0400

    remove image from search results

--- frontend/components/search/search_index_album.jsx
+++ frontend/components/search/search_index_album.jsx
@@ -10,7 +10,6 @@ class SearchIndexAlbum extends React.Component {
     return(
       <li className="search-index-album">
         <p>ALBUM</p>
-        <img className="search-index-album-art" src={this.props.album.artwork} />
         <Link to={`/album/${this.props.album.id}`}>
           <span className="search-index-album-name">{this.props.album.title}</span>
         </Link>
