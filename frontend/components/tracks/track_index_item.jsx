1a0673ebd54ba1ebccee34a7a0b811cdec2c007f
kayandrewj <kayandrewj@gmail.com>
2017-09-13 14:42:22 -0400

    fix tracks playing at the same time

--- frontend/components/tracks/track_index_item.jsx
+++ frontend/components/tracks/track_index_item.jsx
@@ -3,6 +3,21 @@ import React from 'react';
 class TrackIndexItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this._onPlay = this._onPlay.bind(this);
+  }
+
+
+  _onPlay() {
+    let tracks = document.getElementsByTagName('audio');
+    // debugger
+    let i = 0;
+    while (i < tracks.length) {
+      if (tracks[i].currentSrc != this.props.track.audio_file) {
+        tracks[i].pause();
+      }
+      i++;
+    }
   }
 
   render() {
@@ -11,7 +26,7 @@ class TrackIndexItem extends React.Component {
         <span className={this.props.idx === 0 ? "first-list-item track-title" : "track-title"}
 
           >{this.props.track.title}</span>
-        <audio controls>
+        <audio controls onPlay={this._onPlay}>
           <source src={this.props.track.audio_file} type="audio/mp3" />
         </audio>
       </li>
