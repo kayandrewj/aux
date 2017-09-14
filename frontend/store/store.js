43be78c78713a7adcbab906c64ef58a58faedea9
kayandrewj <kayandrewj@gmail.com>
2017-09-14 11:20:16 -0400

    remove debugging material

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -7,8 +7,7 @@ const configureStore = (preloadedState) => {
   return createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
-    // applyMiddleware(thunk)
+    applyMiddleware(thunk)
   );
 };
 
